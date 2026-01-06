9fd75d069c71a2cb3c688c4e7a89e48f7183a9fb
Max_Elder <maxez1999@gmail.com>
2026-01-05 22:03:40 -0300

    Update script.js
    Teste

--- script.js
+++ script.js
@@ -1,52 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const pageContainer = document.querySelector(".container");
+const pinWrap = document.querySelector(".pin-wrap");
 
-/* Locomotive Scroll */
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
-});
+const scrollLength = pinWrap.scrollWidth - window.innerWidth;
 
-/* Sincronização */
-scroller.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight
-    };
-  },
-  pinType: pageContainer.style.transform ? "transform" : "fixed"
-});
-
-/* Horizontal scroll */
-window.addEventListener("load", () => {
-  const pinWrap = document.querySelector(".pin-wrap");
-  const pinWrapWidth = pinWrap.scrollWidth;
-  const horizontalScrollLength = pinWrapWidth - window.innerWidth;
-
-  gsap.to(pinWrap, {
-    x: -horizontalScrollLength,
-    ease: "none",
-    scrollTrigger: {
-      trigger: "#sectionPin",
-      scroller: pageContainer,
-      start: "top top",
-      end: pinWrapWidth,
-      scrub: true,
-      pin: true
-    }
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => scroller.update());
-  ScrollTrigger.refresh();
+gsap.to(pinWrap, {
+  x: -scrollLength,
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#sectionPin",
+    start: "top top",
+    end: () => `+=${pinWrap.scrollWidth}`,
+    scrub: true,
+    pin: true,
+    anticipatePin: 1
+  }
 });
